4a46a34ce59859f8a31950f83d2dd18af46bf226
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-01-08 11:30:20 -0600

    Mastermind Game start

---
+++ src/components/NEWCOMP.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Newcomp(props) {
+    return (
+        <div /* className="Newcomp" id={`newcomp-${props.num}`}*/>
+            <p>Bedroom /*{props.num}*/</p>
+        </div>
+    );
+}
+export default Newcomp;
+
+//Note: in App.js, add import Newcomp from './src/components/Newcomp';
